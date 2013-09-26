2e2221da81e09ac4ba2a7d903069da5b402e4384
wbthomason <wil.thomason@gmail.com>
2013-09-25 21:24:10 -0400

    Made cd and history work.

--- gash.rs
+++ gash.rs
@@ -1,19 +1,38 @@
 use std::{io, run};
+use internal::*;
+mod internal;
 
 fn main() {
     static CMD_PROMPT: &'static str = "gash > ";
-    
+    let mut hist = internal::HistoryLog { history: ~[] };
     loop {
         print(CMD_PROMPT);
         let line = io::stdin().read_line();
         debug!(fmt!("line: %?", line));
         let mut argv: ~[~str] = (line.split_iter(' ').filter(|&x| x != "")).map(|x: &str| x.to_owned()).collect();
         debug!(fmt!("argv %?", argv));
-        
+        hist.history = hist.addhistory(line);
         if argv.len() > 0 {
             let program = argv.remove(0);
             match program {
                 ~"exit"     => {return; }
+                ~"cd"       => {
+                    if argv.len() > 0 {
+                        changedir(argv.remove(0));
+                    }
+                }
+                ~"history"  => {
+                    if argv.len() > 0 {
+                        let arg = argv.remove(0); 
+                        match arg {
+                            ~"-c" => { hist.history = hist.clearhistory(); }
+                            _     => { println(fmt!("Error: %s is not a valid option for history.", arg)); }
+                        }
+                    }
+                    else {
+                        hist.printhistory();
+                    }
+                }
                 _           => {run::process_status(program, argv);}
             }
         }
---
+++ internal.rs
@@ -0,0 +1,45 @@
+extern mod extra;
+use std::{os};
+use self::	extra::{time};
+
+pub fn changedir(pathstr: &str) {
+	let path = &Path(pathstr);
+	if os::path_exists(path) && os::path_is_dir(path) && os::change_dir(path){
+		println(os::getcwd().to_str());
+	}
+	else {
+		println(fmt!("Error: %s is not a valid directory", pathstr));
+		println(fmt!("Current working directory is: %s", os::getcwd().to_str()));
+	}
+}
+
+#[deriving(Clone)]
+pub struct Record {
+	command : ~str,
+	timestamp: time::Tm
+}
+
+pub struct HistoryLog {
+	history: ~[Record]
+}
+
+impl HistoryLog {
+
+	pub fn addhistory(&self, cmd : ~str) -> ~[Record] {
+		let temp = Record {command: cmd, timestamp:time::now()};
+		self.history + ~[temp]
+	}
+
+	pub fn printhistory(&self) {
+		let mut counter = 0;
+		println("\nHistory:\n");
+		for cmd in self.history.iter() {
+			println(fmt!("%d: %s 	%s", counter, cmd.command, cmd.timestamp.rfc822()));
+			counter += 1;
+		}
+	}
+
+	pub fn clearhistory(&self) -> ~[Record]{
+		~[]
+	}
+}
