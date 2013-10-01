5ac172130faf6285cc9ab7a5de805c6ef5111847
MisterAbc <zl4ry@virginia.edu>
2013-09-30 20:52:11 -0400

    final version?

--- gash.rs
+++ gash.rs
@@ -7,14 +7,22 @@ mod command;
 fn main() {
     static CMD_PROMPT: &'static str = "gash";
     let mut hist = internal::HistoryLog { history: ~[] };
+    let mut reexecute = 0;
+    let mut line = ~"";
     command::init();
     loop {
-	print(fmt!("%s: .%s > ", CMD_PROMPT, internal::currentfolder()));
-	let line = io::stdin().read_line();
+	line = match reexecute {
+	    0 => io::stdin().read_line(),
+	    _ => { 
+		print(fmt!("%s: .%s > ", CMD_PROMPT, internal::currentfolder()));
+		line
+	    }
+	};
+	reexecute = 0;
 	debug!(fmt!("line: %?", line));
 	let mut argv: ~[~str] = (line.split_iter(' ').filter(|&x| x != "")).map(|x: &str| x.to_owned()).collect();
 	debug!(fmt!("argv %?", argv));
-	hist.addhistory(line);     
+	hist.addhistory(line.clone());
 	if argv.len() > 0 {
 	    let program = argv.remove(0);
 	    match program {
@@ -32,6 +40,9 @@ fn main() {
 		~"history"  => { 
 		    match argv.shift_opt() {
 			Some(ref x) if x == &~"-c"   => { hist.clearhistory(); }
+			Some(ref x) if x == &~"-l"   => { line = hist.selecthistory();
+							  reexecute = 1;
+							}
 			_                            => { hist.printhistory(); }
 		    }    
 		}
--- internal.rs
+++ internal.rs
@@ -1,51 +1,62 @@
 extern mod extra;
-use std::{os};
+use std::{os, io};
 use self::extra::{time};
+mod command;
 
 pub fn changedir(pathstr: &str) {
-	let path = &Path(pathstr);
-	if !(os::path_exists(path) && os::path_is_dir(path) && os::change_dir(path)){
-		println(fmt!("Error: %s is not a valid directory", pathstr));
-	}
+    let path = &Path(pathstr);
+    if !(os::path_exists(path) && os::path_is_dir(path) && os::change_dir(path)){
+	println(fmt!("Error: %s is not a valid directory", pathstr));
+    }
 }
 
 pub fn currentfolder() -> ~str {
-	let dir = os::getcwd().to_str();
-	let index = match(dir.rfind('/')) {
-					Some(f) => { f }
-					None => { 5 }
-				};
-	dir.slice_from(index).to_owned()
+    let dir = os::getcwd().to_str();
+    let index = match(dir.rfind('/')) {
+	Some(f) => { f }
+	None => { 5 }
+    };
+    dir.slice_from(index).to_owned()
 }
 
 #[deriving(Clone)]
 pub struct Record {
-	command : ~str,
-	timestamp: time::Tm
+    command : ~str,
+    timestamp: time::Tm
 }
 
 pub struct HistoryLog {
-	history: ~[Record]
+    history: ~[Record]
 }
 
 impl HistoryLog {
 
-	pub fn addhistory(&mut self, cmd : ~str) {
-		let temp = Record {command: cmd, timestamp:time::now()};
-		self.history.push(temp);
+    pub fn addhistory(&mut self, cmd : ~str) {
+	let temp = Record {command: cmd, timestamp:time::now()};
+	self.history.push(temp);
+    }
+
+    pub fn printhistory(&self) {
+	let mut counter = 0;
+	println("\nHistory:\n");
+	for cmd in self.history.rev_iter() {
+	    let tabs = "\t".repeat(3-cmd.command.char_len()/8+1);
+	    println(fmt!("%d: %s%s%s", (self.history.len() - counter) as int, cmd.command, tabs, cmd.timestamp.rfc822()));
+	    counter += 1;
+	}
+    }
+
+    pub fn selecthistory(&self) -> ~str {
+	self.printhistory();
+	print("Select a previous command: ");
+	match from_str::<int>(io::stdin().read_line()) {
+	    Some(k)	=> { self.history[k-1].command.clone() }
+	    None	=> { println(fmt!("Bad Input!") ); ~"" }
 	}
 
-	pub fn printhistory(&self) {
-		let mut counter = 0;
-		println("\nHistory:\n");
-		for cmd in self.history.iter() {
-			let tabs = "\t".repeat(3-cmd.command.char_len()/8+1);
-			println(fmt!("%d: %s%s%s", counter, cmd.command, tabs, cmd.timestamp.rfc822()));
-			counter += 1;
-		}
-	}
+    }
 
-	pub fn clearhistory(&mut self) {
-		self.history = ~[]
-	}
+    pub fn clearhistory(&mut self) {
+	self.history = ~[]
+    }
 }
